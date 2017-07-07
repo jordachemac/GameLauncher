5e50d00266fd6401b886eb3057bbe1bf8d67de6a
JordacheMac <jordache.mackenzie@gmail.com>
2017-07-07 23:43:13 +1200

    added local files to version control

---
+++ app/js/tray.js
@@ -0,0 +1,26 @@
+const {remote} = require('electron')
+const {Tray, Menu} = remote
+const path = require('path')
+
+let trayIcon = new Tray(path.join(__dirname, 'icons/png/16x16.png'));
+
+const trayMenuTemplate = [
+   {
+      label: 'Empty Application',
+      enabled: false
+   },
+   {
+      label: 'Settings',
+      click: function () {
+         console.log("Clicked on settings")
+      }
+   },
+   {
+      label: 'Help',
+      click: function () {
+         console.log("Clicked on Help")
+      }
+   }
+]
+let trayMenu = Menu.buildFromTemplate(trayMenuTemplate)
+trayIcon.setContextMenu(trayMenu)
